49b4c538354f3161b797d3206e3ff1ef505e27f5
MarioLFB <popesco19@hotmail.com>
2024-06-05 16:58:50 +0100

    chore: Add about app URL pattern for about page

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from .models import Post
 
 # Create your views here.
 
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
@@ -26,4 +25,9 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render(request, "blog/post_detail.html", {"post": post},)
+    
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
