3f9f8c9cbcdd814887eb5f7e65bdfb6603193ad6
MarioLFB <popesco19@hotmail.com>
2024-06-06 18:22:01 +0100

    chore: Update about_me view to save collaboration requests as unread

--- about/views.py
+++ about/views.py
@@ -2,28 +2,33 @@ from django.shortcuts import render
 from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
+# Create your views here.
+
 
 def about_me(request):
-
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
-            collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+            collaborate_request = collaborate_form.save(commit=False)
+            collaborate_request.read = False
+            collaborate_request.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavor to respond within 2 working days.'
+            )
+            collaborate_form = CollaborateForm()
+        else:
+            collaborate_form = CollaborateForm()
+    else:
+        collaborate_form = CollaborateForm()
 
-    about = About.objects.all().order_by('-updated_on').first()
-    # Instancia o formulário 'CollaborateForm'
-    collaborate_form = CollaborateForm()
-
-    # Combina 'about' e 'collaborate_form' em um único dicionário de contexto
-    context = {
-        "about": about,
-        "collaborate_form": collaborate_form
-    }
-
-    # Passa o objeto request, o caminho do template, e o contexto único para a função 'render'
     return render(
         request,
         "about/about.html",
-        context
+        {"about": about,
+        "collaborate_form": collaborate_form},
     )
